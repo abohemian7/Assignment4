8905d5564adac5c4f5d102c06c879f51b23922a6
Andrew Boyer <boyer.andrew.7@gmail.com>
2015-12-14 02:06:29 -0500

    almost there - need to add the front page shit

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var translator = require('../js/zombie-translator');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  //var intro = fs.pipe("../README.md")
+  //res.send(intro)
   res.render('index', { title: 'Zombie Translator API' });
+  //res.render(markdown({
+  //  directory: '../README.md'
+  //}));
 });
 
 /* GET zombie translation. */
 router.get('/zombify/:zombieWords', function(req, res, next) {
 
-  if(req.params.zombieWords.length >= 1000){
+  if(req.params.zombieWords.length > 1000){
     res.send({ "status": 414, "message": "zombie words are too long" });
   } else{
-    var test = translator.zombify(req.params.zombieWords);
-    console.log(test);
-    res.send('respond with a zombify :: ' + test);
+    var translated = translator.zombify(req.params.zombieWords);
+    res.send({value: translated});
   }
 
 
@@ -24,12 +29,11 @@ router.get('/zombify/:zombieWords', function(req, res, next) {
 /* GET english translation. */
 router.get('/unzombify/:englishWords', function(req, res, next) {
 
-  if(req.params.englishWords.length >= 1000){
+  if(req.params.englishWords.length > 1000){
     res.send({ "status": 414, "message": "english words are too long" });
   } else{
-    var test = translator.unzombify(req.params.englishWords);
-    console.log(test);
-    res.send('respond with a unzombify :: ' + test);
+    var translated = translator.unzombify(req.params.englishWords);
+    res.send({value: translated});
   }
 
 });
