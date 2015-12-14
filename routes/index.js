9a40a2f1acb45da31562ae307ee34a76e2fcce52
Andrew Boyer <boyer.andrew.7@gmail.com>
2015-12-14 01:04:55 -0500

    routes working and all

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,40 @@ var translator = require('../js/zombie-translator');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Zombie Translator API' });
 });
 
 /* GET zombie translation. */
-router.get('/zombify', function(req, res, next) {
-  res.send('respond with a zombify');
+router.get('/zombify/:zombieWords', function(req, res, next) {
+
+  if(req.params.zombieWords.length >= 1000){
+    res.sendStatus(414);
+  } else{
+    var test = translator.zombify(req.params.zombieWords);
+    console.log(test);
+    res.send('respond with a zombify :: ' + test);
+  }
+
+
 });
 
 /* GET english translation. */
-router.get('/unzombify', function(req, res, next) {
-  var test = translator.zombify('hello');
-  console.log(test);
-  res.send('respond with a unzombify :: ' + test);
+router.get('/unzombify/:englishWords', function(req, res, next) {
+
+  if(req.params.englishWords.length >= 1000){
+    res.sendStatus(414);
+  } else{
+    var test = translator.unzombify(req.params.englishWords);
+    console.log(test);
+    res.send('respond with a unzombify :: ' + test);
+  }
+
 });
+
+router.get('*', function(req, res){
+  res.sendStatus(404);
+})
+
+
 
 module.exports = router;
